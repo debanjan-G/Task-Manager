2ea68d05b0abbb26dff6ec025ad48566e68e825f
Debanjan <debanjanghosal10@gmail.com>
2024-07-12 17:19:32 +0530

    Created create-new-task route of API

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,10 +1,43 @@
+import { connectDB } from "@/lib/connectDB";
+import TaskList from "@/lib/models/task-list-model";
 import { NextResponse } from "next/server";
+import Task from "@/lib/models/task-model";
 
-export async function GET(req) {
+export async function POST(req) {
   try {
-    return NextResponse.json({ msg: "Fetching all tasks" });
+    await connectDB();
+    const reqBody = await req.json();
+
+    console.log(reqBody);
+
+    const taskListName = reqBody.taskList;
+    const task = reqBody.task;
+
+    // Searching the TaskList collection for a document with the taskListName
+    const taskList = await TaskList.findOne({ title: taskListName });
+    if (!taskList) {
+      return NextResponse.json(
+        { success: false, msg: "No such list found" },
+        { status: 201 }
+      );
+    }
+
+    // Creating a new Task document
+    const newTask = new Task({
+      taskList: taskList._id,
+      task,
+    });
+
+    // saving the document
+    await newTask.save();
+
+    console.log(reqBody);
+    return NextResponse.json(
+      { success: true, savedTask: newTask },
+      { status: 201 }
+    );
   } catch (error) {
-    console.log("ERROR");
-    return NextResponse.json({ message: "Something Went Wrong", error });
+    console.log("Error: ", error);
+    return NextResponse.json({ error });
   }
 }
