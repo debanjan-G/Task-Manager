9fb39c5eefceac9eb0730441d692d46b23d58ffe
Debanjan <debanjanghosal10@gmail.com>
2024-07-13 21:52:39 +0530

    Added loading UI each task list component

---
+++ src/app/todo-list/[listName]/loading.js
@@ -0,0 +1,12 @@
+import Loading from "@/components/ui/Loading/Loading";
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex h-[90vh] items-center justify-center">
+      <Loading />
+    </div>
+  );
+};
+
+export default loading;
