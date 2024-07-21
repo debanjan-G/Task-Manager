dc058475c9ae95885f473dd37e27119c16ba4983
Debanjan <debanjanghosal10@gmail.com>
2024-07-21 18:53:45 +0530

    Created Backend API Route for getting all tasklists of an user

--- src/app/api/taskList/route.js
+++ src/app/api/taskList/route.js
@@ -1,8 +1,33 @@
 import { connectDB } from "@/lib/connectDB";
 import TaskList from "@/lib/models/task-list-model";
+import User from "@/lib/models/user-model";
 import { NextResponse } from "next/server";
 
+// Get all Task Lists of a particular author/user
 
+export async function GET(req) {
+  try {
+    const queryParams = req.nextUrl.searchParams;
+    const authorName = queryParams.get("author");
+    // Get the author ID
+    const author = await User.findOne({ name: authorName });
+    // Find all tasklists created by the author
+    const taskLists = await TaskList.find({ author: author._id });
+    return NextResponse.json({
+      success: true,
+      count: taskLists.length,
+      taskLists,
+    });
+  } catch (error) {
+    console.log("ERROR: ", error);
+    return NextResponse.json(
+      {
+        msg: "Oops! Something Went Wrong. Check server console.",
+      },
+      { status: 500 }
+    );
+  }
+}
 
 // Create a New Task List
 export async function POST(req) {
