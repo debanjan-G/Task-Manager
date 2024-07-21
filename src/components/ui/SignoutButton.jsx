4aa505560d9a7c077b91acf20c66c51dbdc2526a
Debanjan <debanjanghosal10@gmail.com>
2024-07-21 22:01:01 +0530

    Added alert dialog component to logout

--- src/components/ui/SignoutButton.jsx
+++ src/components/ui/SignoutButton.jsx
@@ -1,28 +1,56 @@
 import React from 'react'
 import { auth, signOut } from '@/auth';
 import LogoutSVG from './LogoutSVG';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
 
 
 const SignoutButton = async () => {
     // const router = useRouter()
     const session = await auth();
+
     return (
-
-        <span className='text-right'>
-            {session && session.user ?
-                <form action={async () => {
-                    'use server'
-                    await signOut();
-                    // router.push('/')
-                }}>
-                    <button type='submit'>
+        <span className='flex justify-end'>
+            {session && session.user &&
+                <AlertDialog>
+                    <AlertDialogTrigger>
                         <LogoutSVG />
-                    </button>
-                </form>
-                :
-                null}
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Are you sure you want to log out? Your unsaved changes may be lost.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
 
-        </span>
+                            <form action={async () => {
+                                'use server'
+                                await signOut();
+                                // router.push('/')
+                            }}>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <button type='submit'>
+                                    <AlertDialogAction className='bg-red-500 hover:bg-red-700'>Continue</AlertDialogAction>
+                                </button>
+                            </form>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+            }
+
+        </span >
     )
 }
 
